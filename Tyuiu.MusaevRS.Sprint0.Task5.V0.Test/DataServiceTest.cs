f2b446b5d2333531a52bef68e450905738fca301
Руслан Мусаев <ru.mu2005@mail.ru>
2023-10-01 23:58:06 +0500

    Task5

---
+++ Tyuiu.MusaevRS.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MusaevRS.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MusaevRS.Sprint0.Task5.V0.Test
+{
+
+
+        [TestClass]
+        public class DataServiceTest
+        {
+            [TestMethod]
+            public void CheckAdditionValid()
+            {
+                Assert.AreEqual(10, DataService.Addition(5, 5));
+            }
+            [TestMethod]
+            public void CheckSubstructionValid()
+            {
+                Assert.AreEqual(5, DataService.Substruction(10, 5));
+            }
+            [TestMethod]
+            public void CheckMultiplicationValid()
+            {
+                Assert.AreEqual(50, DataService.Multiplication(10, 5));
+            }
+            [TestMethod]
+            public void CheckDivisionValid()
+            {
+                Assert.AreEqual(3, DataService.Division(9, 3));
+            }
+        }
+    }
+
